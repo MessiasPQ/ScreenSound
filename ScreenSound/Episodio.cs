3199905b047155da2c46582a55eb9754c76cfb56
MessiasPQ <messias1572@hotmail.com>
2023-08-29 21:08:35 -0300

    Criando construtor da classe Episodio

--- ScreenSound/Episodio.cs
+++ ScreenSound/Episodio.cs
@@ -1,15 +1,15 @@
 ﻿class Episodio{
-    public int Duracao { get; set; }
-    public int Ordem { get; set; }
-    public string Resumo { get; set; }
-    public string Titulo { get; set; }
+    public int Duracao { get; }
+    public int Ordem { get; }
+    public string Resumo => $"{Ordem}. {Titulo} ({Duracao} min) ";
+    public string Titulo { get; }
 
-    public Episodio()
+    private List<string> convidados = new();
+    public Episodio(int duracao, int ordem, string titulo)
     {
-        Duracao = Duracao;
-        Ordem = Ordem;
-        Resumo = Resumo;
-        Titulo = Titulo;
+        Duracao = duracao;
+        Ordem = ordem;
+        Titulo = titulo;
     }
 
     public void AdicionarConvidados()
